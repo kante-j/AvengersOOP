47e12ad74014415a9cd48cc13b8e8ff4d4194eb3
wltjr0920 <wltjr0920@ajou.ac.kr>
2017-05-27 16:48:26 +0900

    3

--- AvengersOOP/src/avengers/Menu.java
+++ AvengersOOP/src/avengers/Menu.java
@@ -5,52 +5,45 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.*;
 
-
 public class Menu extends JPanel {
-	
-	public boolean started = false;
-	
 
-	
-	public Menu(){
-		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
-		
+	public boolean started = false;
+
+	public Menu() {
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
 		add(Box.createVerticalStrut(280));
-		
+
 		Custombutton button = new Custombutton("START");
-		
+
 		button.setOpaque(false);
-		
+
 		button.setAlignmentX(CENTER_ALIGNMENT);
-		button.addActionListener(new AbstractAction(){
-				public void actionPerformed(ActionEvent e){
-					start();
-					
-					new Story();
-				}
-				
+		button.addActionListener(new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				start();
+
+				new Story();
+			}
+
 		});
-		
-		
+
 		add(button);
-		
+
 		add(Box.createVerticalGlue());
-			
+
 	}
-	
+
 	public void start() {
 		started = true;
 	}
-	
-	
-	@Override
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		
-			g.drawImage(new ImageIcon(Menu.class.getResource("avengers_background.jpg")).getImage(), 0,0,640,480,this);
-		
-			
-		}
-	}
-	
 
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Image img = new ImageIcon("avengers_background.jpg").getImage();
+		g.drawImage(img, 0, 0, 640, 480, null);
+
+
+	}
+}
